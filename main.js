47b9dcf9bc7010794f5bf3e9e5242c8e5ba75ee6
Shahnawaz <brandgains@gmail.com>
2024-10-08 12:45:01 +0530

    Responsivness Issues, Noise overlay responsive

--- main.js
+++ main.js
@@ -49,151 +49,148 @@ document.addEventListener("DOMContentLoaded", () => {
 
   move();
 
-  // ScrollTrigger Text Reveal Animation
-  const splitTypes = document.querySelectorAll(".reveal-type");
-  splitTypes.forEach((words, i) => {
-    const text = new SplitType(words, {
-      types: "words",
-    });
+  // Check screen width and apply animations conditionally
+  if (window.innerWidth > 768) {
+    // ScrollTrigger Text Reveal Animation
+    const splitTypes = document.querySelectorAll(".reveal-type");
+    splitTypes.forEach((words, i) => {
+      const text = new SplitType(words, {
+        types: "words",
+      });
 
-    gsap.from(text.words, {
-      scrollTrigger: {
-        trigger: words,
-        start: "top 80%",
-        end: "top 20%",
-        scrub: true,
-        // markers: true,
-      },
-      mixBlendMode: "overlay",
-      // opacity: .2,
-      stagger: 0.1,
-    });
-  });
-
-  // Works Heading Animation
-  const worksHeadings = document.querySelectorAll(".works-heading");
-  worksHeadings.forEach((chars, i) => {
-    const text = new SplitType(chars, {
-      types: "chars",
-    });
-
-    gsap.fromTo(
-      text.chars,
-      {
-        y: "100%",
-        opacity: 0,
-      },
-      {
-        y: "0%",
-        opacity: 1,
+      gsap.from(text.words, {
         scrollTrigger: {
-          trigger: chars,
-          start: "top 75%",
-          end: "top 35%",
-          scrub: true,
-          // markers: true
-        },
-        stagger: 0.1,
-        ease: "power1.in",
-        onComplete: () => {
-          gsap.to(document.querySelectorAll(".work-heading-wrapper p"), {
-            opacity: 1,
-            duration: 0.8,
-            ease: "power1.in",
-          });
-        },
-      }
-    );
-  });
-
-  // Work Cards Animation
-  const workCards = document.querySelectorAll(".work-card");
-
-  workCards.forEach((card, i) => {
-    const image = card.querySelector("img");
-    const h4 = card.querySelector("h4");
-    image.style.willChange = "transform, opacity";
-    h4.style.willChange = "opacity";
-
-    gsap.fromTo(
-      image,
-      {
-        // scale: 0,
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        scrollTrigger: {
-          trigger: card,
+          trigger: words,
           start: "top 80%",
-          end: "top 30%",
+          end: "top 20%",
           scrub: true,
+          // markers: true,
         },
-      }
-    );
+        mixBlendMode: "overlay",
+        // opacity: .2,
+        stagger: 0.1,
+      });
+    });
 
-    gsap.fromTo(
-      h4,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        scrollTrigger: {
-          trigger: card,
-          start: "top 70%",
-          end: "top 30%",
-          scrub: true,
+    // Works Heading Animation
+    const worksHeadings = document.querySelectorAll(".works-heading");
+    worksHeadings.forEach((chars, i) => {
+      const text = new SplitType(chars, {
+        types: "chars",
+      });
+
+      gsap.fromTo(
+        text.chars,
+        {
+          y: "100%",
+          opacity: 0,
         },
-      }
-    );
+        {
+          y: "0%",
+          opacity: 1,
+          scrollTrigger: {
+            trigger: chars,
+            start: "top 75%",
+            end: "top 35%",
+            scrub: true,
+            // markers: true
+          },
+          stagger: 0.1,
+          ease: "power1.in",
+          onComplete: () => {
+            gsap.to(document.querySelectorAll(".work-heading-wrapper p"), {
+              opacity: 1,
+              duration: 0.8,
+              ease: "power1.in",
+            });
+          },
+        }
+      );
+    });
+
+    // Work Cards Animation
+    const workCards = document.querySelectorAll(".work-card");
+
+    workCards.forEach((card, i) => {
+      const image = card.querySelector("img");
+      const h4 = card.querySelector("h4");
+      image.style.willChange = "transform, opacity";
+      h4.style.willChange = "opacity";
+
+      gsap.fromTo(
+        image,
+        {
+          // scale: 0,
+          opacity: 0,
+        },
+        {
+          opacity: 1,
+          scrollTrigger: {
+            trigger: card,
+            start: "top 80%",
+            end: "top 30%",
+            scrub: true,
+          },
+        }
+      );
+
+      gsap.fromTo(
+        h4,
+        {
+          opacity: 0,
+        },
+        {
+          opacity: 1,
+          scrollTrigger: {
+            trigger: card,
+            start: "top 70%",
+            end: "top 30%",
+            scrub: true,
+          },
+        }
+      );
+    });
+  }
+
+  // Work Card Hover Animation
+  const workCards = document.querySelectorAll(".work-card");
+  workCards.forEach((card) => {
+    const image = card.querySelector("img");
+    const link = card.querySelector("h4 a");
+    const h4 = card.querySelector("h4");
+
+    // Image hover events
+    image.addEventListener("mouseenter", () => {
+      cursor.setText("Click");
+      cursor.show();
+      document.body.style.cursor = "none";
+    });
+
+    image.addEventListener("mouseleave", () => {
+      cursor.removeText();
+      // cursor.hide();
+      document.body.style.cursor = "default";
+    });
+
+    h4.addEventListener("mouseenter", () => {
+      cursor.setText("Click");
+      cursor.show();
+      document.body.style.cursor = "none";
+    });
+
+    h4.addEventListener("mouseleave", () => {
+      cursor.removeText();
+      // cursor.hide();
+      document.body.style.cursor = "default";
+    });
+
+    image.addEventListener("click", () => {
+      link.click();
+    });
   });
-});
 
-// Work Card Hover Animation
-const workCards = document.querySelectorAll(".work-card");
-workCards.forEach((card) => {
-  const image = card.querySelector("img");
-  const link = card.querySelector("h4 a");
-  const h4 = card.querySelector("h4");
-
-  // Image hover events
-  image.addEventListener("mouseenter", () => {
-    cursor.setText("Click");
-    cursor.show();
-    document.body.style.cursor = "none";
-  });
-
-  image.addEventListener("mouseleave", () => {
-    cursor.removeText();
-    // cursor.hide();
-    document.body.style.cursor = "default";
-  });
-
-  h4.addEventListener("mouseenter", () => {
-    cursor.setText("Click");
-    cursor.show();
-    document.body.style.cursor = "none";
-  });
-
-  h4.addEventListener("mouseleave", () => {
-    cursor.removeText();
-    // cursor.hide();
-    document.body.style.cursor = "default";
-  });
-
-  image.addEventListener("click", () => {
-    link.click();
-  });
-});
-
-
-
-
-// scroll arrow for mobile devices 
-document.addEventListener("DOMContentLoaded", () => {
+  // Scroll arrow for mobile devices
   const scrollArrow = document.querySelector(".scroll-arrow");
-
   function handleResize() {
     if (window.innerWidth <= 768) {
       if (scrollArrow) {
@@ -210,6 +207,5 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   handleResize();
-
   window.addEventListener("resize", handleResize);
 });
